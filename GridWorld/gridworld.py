70bec3f3933c2431d691866ac56e29a02c8c892f
Andrew <amelnychukoseen@gmail.com>
2018-06-17 12:05:04 -0700

    Copying in grid world code from tutorial.

---
+++ GridWorld/gridworld.py
@@ -0,0 +1,49 @@
+
+
+import numpy as np
+
+class Coordinate:
+    def __init__(self):
+
+        self.i = 0
+        self.j = 0
+
+class Grid:
+    def __init__(self, width, height, start):
+        self._width = width
+        self._height = height
+        self.i = start[0]
+        self.j = start[1]
+
+    def set(self, rewards, actions):
+        self._rewards = rewards
+        self._actions = actions
+
+    def set_state(self, s):
+        self.i = s[0]
+        self.j = s[1]
+
+    def current_state(self):
+        return (self.i, self.j)
+
+    def is_terminal(self, s):
+        return s not in self._actions
+
+    def move(self, action):
+        if action in self._actions[(self.i, self.j)]:
+            if action == 'U':
+                self.i -= 1
+            if action == 'D':
+                self.i += 1
+            if action == 'R':
+                self.j += 1
+            if action == 'L':
+                self.j -= 1
+
+        assert(self.current_state() in self.all_states())
+
+    def game_over(self):
+        return (self.i, self.j) not in self._actions
+
+    def all_states(self):
+        return set(self._actions.keys()) | set(self._rewards.keys())
